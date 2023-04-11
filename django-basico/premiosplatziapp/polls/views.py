0fb0ed29891363dfeceff3332e1a6669a758e974
Kevin Gutierrez <harold.dev1993@gmail.com>
2023-04-10 20:31:06 -0500

    avanzando ando

--- django-basico/premiosplatziapp/polls/views.py
+++ django-basico/premiosplatziapp/polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estás en la página principal de PREMIOS PLATZI APP")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta # {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta # {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta # {question_id}")
